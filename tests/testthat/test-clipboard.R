258e0807568e7994f7ec19a93d4049c8ea446987
MS Berends <31037261+msberends@users.noreply.github.com>
2018-03-29 14:26:26 +0200

    Update test-clipboard.R

--- tests/testthat/test-clipboard.R
+++ tests/testthat/test-clipboard.R
@@ -2,8 +2,10 @@ context("clipboard.R")
 
 test_that("clipboard works", {
   # why is the <<- needed? Won't work without it...
-  t1 <<- AMR::antibiotics
-  clipboard_export(t1, info = FALSE)
-  t2 <- clipboard_import()
-  expect_equal(t1, t2)
+  if (Sys.info()['sysname'] %like% "Windows") {
+    t1 <<- AMR::antibiotics
+    clipboard_export(t1, info = FALSE)
+    t2 <- clipboard_import()
+    expect_equal(t1, t2)
+  }
 })
