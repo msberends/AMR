e2a5202b6900922cedc433f4ca241289235a9bdd
MS Berends <31037261+msberends@users.noreply.github.com>
2018-03-29 14:47:52 +0200

    Update test-clipboard.R

--- tests/testthat/test-clipboard.R
+++ tests/testthat/test-clipboard.R
@@ -1,11 +1,9 @@
 context("clipboard.R")
 
 test_that("clipboard works", {
-  # why is the <<- needed? Won't work without it...
-  if (Sys.info()['sysname'] %like% "Windows") {
-    t1 <<- AMR::antibiotics
-    clipboard_export(t1, info = FALSE)
-    t2 <- clipboard_import()
-    expect_equal(t1, t2)
-  }
+  skip_on_os(c("linux", "solaris"))
+  t1 <<- AMR::antibiotics # why is the <<- needed? Won't work without it...
+  clipboard_export(t1, info = FALSE)
+  t2 <- clipboard_import()
+  expect_equal(t1, t2)
 })
