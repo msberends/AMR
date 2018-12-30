df6b334c6fe5e5f308d14aea985e537cbc04a713
Matthijs S. Berends <m.s.berends@umcg.nl>
2018-12-30 10:27:28 +0100

    website update

--- docs/extra.js
+++ docs/extra.js
@@ -1,3 +1,11 @@
 // Add updated Font Awesome 5.6.3 library
 $('head').append('<!-- Updated Font Awesome library --><link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">');
-$('footer p').text($('footer p').text().replace('Developed by', 'Authors:'));
+
+/* edit footer */
+$( document ).ready(function() {
+  $('footer').html('<p>' +
+                   $('footer .copyright p').html().replace("Developed by",
+                                                           "<code>AMR</code> (for R). Developed at the University of Groningen.<br>Authors:") +
+                   '</p>');
+//$('footer').prepend("<div class='university'/>");
+});
--- pkgdown/extra.js
+++ pkgdown/extra.js
@@ -2,8 +2,10 @@
 $('head').append('<!-- Updated Font Awesome library --><link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">');
 
 /* edit footer */
-$('footer').html('<p>' +
-                 $('footer .copyright p').html().replace("Developed by",
-                                                         "<code>AMR</code> (for R). Developed at the University of Groningen.<br>Authors:") +
-                 '</p>');
+$( document ).ready(function() {
+  $('footer').html('<p>' +
+                   $('footer .copyright p').html().replace("Developed by",
+                                                           "<code>AMR</code> (for R). Developed at the University of Groningen.<br>Authors:") +
+                   '</p>');
 //$('footer').prepend("<div class='university'/>");
+});
